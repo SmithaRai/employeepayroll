587338b8e5bd68a24ec2cf42ca1e9c947718544f
unknown <SmithaRai.com>
2022-04-30 16:17:26 +0530

    Added : added println statement

---
+++ src/com/bridgelabz/employeepayroll/EmployeePayRoll.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayRoll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("------------***EMPLOYEE PAY ROLL USING FILE HANDLING***-----------------");
+	}
+
+}
